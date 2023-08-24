c0c91e3779d063bcc5ba5e89f30cadd641bcfa04
RussianSpytato <101286294+RussianSpytato@users.noreply.github.com>
2023-08-24 10:44:24 -0700

    Add files via upload

--- setupscript/setup.sh
+++ setupscript/setup.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-mv .xinitrc ~/.xinitrc
-echo "amixer set 'PGA1.0 1 Master' 1%+" > /sbin/upvol
-echo "amixer set 'PGA1.0 1 Master' 1%-" > /sbin/downvol
-echo "amixer set 'PGA1.0 1 Master' 0" > /sbin/mutevol
-git clone https://github.com/eupnea-linux/audio-scripts
-cd audio-scripts
+#!/bin/sh
+cp .xinitrc /home/*/.xinitrc
+sudo cp scripts/* /bin/*
+cd chromebook-linux-audio
 ./setup-audio
