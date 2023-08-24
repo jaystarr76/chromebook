9eb3a7bfd0211fb463cfa820a5b039a85b9dd1aa
RussianSpytato <101286294+RussianSpytato@users.noreply.github.com>
2023-08-24 10:38:07 -0700

    Add files via upload
    updated config with nord theme

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -3,24 +3,24 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Libreation Mono:size=16"
-};
+static const char *fonts[] = {	"Liberation Mono:size=16" };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+
+	[SchemeNorm] = { "#ECEFF4", "#2E3440" },
+	[SchemeSel]  = {"#2E3440" , "#88C0D0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* dmx is pixels from left 2 right, dmy is pixels from top to bottom, dmw is width in pixels, lineheight is height in pixels */
-static unsigned int lineheight = 32;
+static unsigned int lineheight = 33;
 static unsigned int min_lineheight =32;
-static int dmx = 10;
-static int dmy = 10;
-static int dmw = 1346;
+static int dmx = 0;
+static int dmy = 0;
+static int dmw = 1366;
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
